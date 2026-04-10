b6595e51f9914c0f2feed1e3ca948f59f1afc5e0
GradeACan <julien20007mareil@gmail.com>
2026-04-10 09:44:47 +0200

    i read about mmap, is it better? ssems so, now i have no major page faults

--- C?/newtest.c
+++ C?/newtest.c
@@ -5,30 +5,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <sys/mman.h>
 
 void parallel_sort_bridge(unsigned int* arr,size_t size); //CPP sort algo
 
 int main() {
-    unsigned int *arr = malloc(1000000000*sizeof(unsigned int));
+    uint32_t *arr = mmap(NULL,1000000000*sizeof(unsigned int), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE, -1, 0);
+    madvise(arr, 1000000000*sizeof(unsigned int), MADV_SEQUENTIAL | MADV_WILLNEED);
+
     size_t num_elements = 1000000000;
     uint64_t state = 0x1234567890ABCDEF;
 
-    if (arr==NULL) {
-        printf("PC out of memory, Malloc failed!\n");
+    if (arr==MAP_FAILED) {
+        printf("PC out of memory, Mmap failed!\n");
         return 1;
+        exit(1);
     }
 
-    for (size_t i = 0; i < num_elements; i++){
+    for (size_t i = 0; i < num_elements; i +=2){
         state ^= state << 13;
         state ^= state >> 7;
         state ^= state << 17;
         arr[i] = (unsigned int)state;
+        arr[i+1] = (uint32_t)(state>>32);
     }
 
     parallel_sort_bridge(arr, 1000000000); //here last
 
     printf("Array sorted, first is %u, last is %u\n",arr[0],arr[num_elements-1]);
 
-    free(arr);
+    munmap(arr,1000000000*sizeof(unsigned int));
     return 0;
+
+    
 }
+
